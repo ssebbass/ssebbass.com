bc2bb1ebef969b3ce1f76aa3cf9668aa4ab9cea3
Sebastian Juarez <ssebbass@gmail.com>
2016-10-08 14:17:48 -0300

    Update route 53 DNS record to new published IPFS address.

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+set -xe
 
 # Update gravatar
 echo downloading gravatar img...
 ./gravatar.sh ssebbass@gmail.com
 
 # Build & Publish
-jekyll build && ipfs name publish $(ipfs add -rq _site/ | tail -1 ) | tee publish
+jekyll build
+PUBHASH=$(ipfs add -rq _site/ | tail -1 )
+sed -i.bak 's/.*Value.*/\"Value\"\:\ \"\\"dnslink\=\/ipfs\/'$PUBHASH'\\"\"/' www.ssebbass.com-txt.json
+ZONEID=$(aws route53 list-hosted-zones-by-name --dns-name ssebbass.com. --profile ssebbass | grep Id | tr -d \" | tr -d \, | awk -F \/ '{print $3}')
+aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://./www.ssebbass.com-txt.json --profile ssebbass
