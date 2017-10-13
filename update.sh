d2ef80237bba0c1f56ffa769122bfe06b69b9dc7
Juarez Sebastian <sjuarez@amco.mx>
2017-10-13 16:27:02 -0300

    Updated

--- update.sh
+++ update.sh
@@ -6,7 +6,8 @@ echo downloading gravatar img...
 ./gravatar.sh ssebbass@gmail.com
 
 # Build & Publish
-jekyll build
+bundle install
+bundle exec jekyll build
 PUBHASH=$(ipfs add -rq _site/ | tail -1 )
 sed -i.bak 's/.*Value.*/\"Value\"\:\ \"\\"dnslink\=\/ipfs\/'$PUBHASH'\\"\"/' www.ssebbass.com-txt.json
 ZONEID=$(aws route53 list-hosted-zones-by-name --dns-name ssebbass.com. --profile ssebbass | grep Id | tr -d \" | tr -d \, | awk -F \/ '{print $3}')
